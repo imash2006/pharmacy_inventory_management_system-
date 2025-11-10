b3cc239e605412e2157e36a33bd8f869c039dcd2
imash2006 <imashkottage24@gmail.com>
2025-11-10 22:56:15 +0530

    Adding Login_form.fxml and the controller class

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Starter extends Application {
@@ -7,6 +9,7 @@ public class Starter extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
-
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/login_form.fxml"))));
+        stage.show();
     }
 }
