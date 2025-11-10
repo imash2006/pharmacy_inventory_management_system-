d0cfbea14b11cb88f6658ac567297603145de1d0
imash2006 <imashkottage24@gmail.com>
2025-11-10 22:52:18 +0530

    Adding Login_form.fxml and the controller class

---
+++ src/main/java/controller/LoginFormController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+
+    @FXML
+    private Button btnlogin;
+
+    @FXML
+    private PasswordField txtpassword;
+
+    @FXML
+    private TextField txtusername;
+
+    @FXML
+    void btnloginOnAction(ActionEvent event) {
+
+    }
+
+}
