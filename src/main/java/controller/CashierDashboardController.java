6cb765717abdce09662b8364836b07b71e1d1816
imash2006 <imashkottage24@gmail.com>
2025-11-17 23:48:28 +0530

    cashier dashboard and order details fxml update, cashier dashboard controller update

---
+++ src/main/java/controller/CashierDashboardController.java
@@ -0,0 +1,149 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class CashierDashboardController {
+
+    @FXML
+    private Button btnAddToCart;
+
+    @FXML
+    private Button btnDashboard;
+
+    @FXML
+    private ImageView btnLogout;
+
+    @FXML
+    private Button btnMedicines;
+
+    @FXML
+    private Button btnNotification;
+
+    @FXML
+    private Button btnOrder;
+
+    @FXML
+    private Button btnPlaceOrder;
+
+    @FXML
+    private Button btnPointOfSale;
+
+    @FXML
+    private Button btnSupplier;
+
+    @FXML
+    private Button btnViewItemTable;
+
+    @FXML
+    private Button btnlogout;
+
+    @FXML
+    private Label idCategory;
+
+    @FXML
+    private Label idDate;
+
+    @FXML
+    private Label idExpiringSoon;
+
+    @FXML
+    private Label idExpiryDate;
+
+    @FXML
+    private Label idLowStockItems;
+
+    @FXML
+    private Label idName;
+
+    @FXML
+    private ImageView idNotificationBell;
+
+    @FXML
+    private Label idPrice;
+
+    @FXML
+    private Label idTime;
+
+    @FXML
+    private Label idTodaysSales;
+
+    @FXML
+    private Label idTotalNotifications;
+
+    @FXML
+    private TableView<?> tdlCartItemList;
+
+    @FXML
+    private TextField txtMedicineId;
+
+    @FXML
+    private TextField txtQuantity;
+
+    @FXML
+    void btnAddToCartOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnMedicineOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNotificationOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlaceOrderOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPointOfSaleOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnViewItemTableOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnlogoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tblCartItemList(ActionEvent event) {
+
+    }
+
+}
