b284332a932f29d833788c0f36b46c8e11506827
imash2006 <imashkottage24@gmail.com>
2025-11-18 01:43:47 +0530

    Dashboard and medicine inventory fxml update, Dashboard controller update

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -1,4 +1,101 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+
 public class DashboardController {
+
+    @FXML
+    private Button btnDashboard;
+
+    @FXML
+    private Label btnExpiringSoon;
+
+    @FXML
+    private Button btnLogout;
+
+    @FXML
+    private Button btnMedicines;
+
+    @FXML
+    private Button btnNotification;
+
+    @FXML
+    private Button btnOrders;
+
+    @FXML
+    private Button btnPointOfSale;
+
+    @FXML
+    private Button btnSupplier;
+
+    @FXML
+    private Label btnTodaysSales;
+
+    @FXML
+    private Label idDate;
+
+    @FXML
+    private ImageView idExpiryAlert;
+
+    @FXML
+    private ImageView idLowStock;
+
+    @FXML
+    private Label idLowStockItems;
+
+    @FXML
+    private ImageView idNotificationBell;
+
+    @FXML
+    private ImageView idSupplierUpdate;
+
+    @FXML
+    private Label idTime;
+
+    @FXML
+    private Label idTotalMedicines;
+
+    @FXML
+    private TableView<?> tblInventoryOverview;
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMedicinesOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNotificationOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrdersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPointOfSaleOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierOnAction(ActionEvent event) {
+
+    }
+
 }
