088c1ba7f4680ba7fd97faf33587a1f003cbadfd
imash2006 <imashkottage24@gmail.com>
2025-11-20 13:32:45 +0530

    order_details.fxml update and OrderDetailsController class update

--- src/main/java/controller/OrderDetailsController.java
+++ src/main/java/controller/OrderDetailsController.java
@@ -2,12 +2,18 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class OrderDetailsController {
 
@@ -85,42 +91,111 @@ public class OrderDetailsController {
 
     @FXML
     void btnDashboardOnAction(ActionEvent event) {
+        idMainPane.getChildren().clear();
 
+        AnchorPane newPane = null;
+        try {
+            newPane = FXMLLoader.load(getClass().getResource("/view/dashboard.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        AnchorPane.setTopAnchor(newPane, 0.0);
+        AnchorPane.setBottomAnchor(newPane, 0.0);
+        AnchorPane.setLeftAnchor(newPane, 0.0);
+        AnchorPane.setRightAnchor(newPane, 0.0);
+        idMainPane2.getChildren().setAll(newPane);
     }
 
     @FXML
     void btnGetCopyOnAction(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/product_order_history.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    Stage stage = new Stage();
     @FXML
     void btnLogoutOnAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/login_form.fxml"))));
 
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnNotificationOnAction(ActionEvent event) {
+        idMainPane.getChildren().clear();
 
-    }
-
-    @FXML
-    void btnNotificationsOnAction(ActionEvent event) {
-
+        AnchorPane newPane = null;
+        try {
+            newPane = FXMLLoader.load(getClass().getResource("/view/notification_overview.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        AnchorPane.setTopAnchor(newPane, 0.0);
+        AnchorPane.setBottomAnchor(newPane, 0.0);
+        AnchorPane.setLeftAnchor(newPane, 0.0);
+        AnchorPane.setRightAnchor(newPane, 0.0);
+        idMainPane2.getChildren().setAll(newPane);
     }
 
     @FXML
     void btnOrdersOnAction(ActionEvent event) {
+        idMainPane.getChildren().clear();
+
+        AnchorPane newPane = null;
+        try {
+            newPane = FXMLLoader.load(getClass().getResource("/view/order_details.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        AnchorPane.setTopAnchor(newPane, 0.0);
+        AnchorPane.setBottomAnchor(newPane, 0.0);
+        AnchorPane.setLeftAnchor(newPane, 0.0);
+        AnchorPane.setRightAnchor(newPane, 0.0);
+        idMainPane2.getChildren().setAll(newPane);
 
     }
 
     @FXML
     void btnPointOfSaleOnAction(ActionEvent event) {
+        idMainPane.getChildren().clear();
 
+        AnchorPane newPane = null;
+        try {
+            newPane = FXMLLoader.load(getClass().getResource("/view/cashier_dashboard.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        AnchorPane.setTopAnchor(newPane, 0.0);
+        AnchorPane.setBottomAnchor(newPane, 0.0);
+        AnchorPane.setLeftAnchor(newPane, 0.0);
+        AnchorPane.setRightAnchor(newPane, 0.0);
+        idMainPane2.getChildren().setAll(newPane);
     }
 
     @FXML
     void btnSuppliersOnAction(ActionEvent event) {
+        idMainPane.getChildren().clear();
 
+        AnchorPane newPane = null;
+        try {
+            newPane = FXMLLoader.load(getClass().getResource("/view/supplier_overview.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        AnchorPane.setTopAnchor(newPane, 0.0);
+        AnchorPane.setBottomAnchor(newPane, 0.0);
+        AnchorPane.setLeftAnchor(newPane, 0.0);
+        AnchorPane.setRightAnchor(newPane, 0.0);
+        idMainPane2.getChildren().setAll(newPane);
     }
 
     @FXML
